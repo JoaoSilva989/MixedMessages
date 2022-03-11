1702b9ce13c7400abe52960922116e6b0290c483
João Silva <JoaoSilva989@users.noreply.github.com>
2022-03-11 17:44:50 +0000

    change

--- MixedMessages.js
+++ MixedMessages.js
@@ -6,7 +6,8 @@ const places = ['Mars',
     'Springfield',
     'King\'s Landing',
     'Atlantis',
-    'Hell']
+    'Hell',
+    'Gotham']
 const actions = [
     'getting hit by a vehicle',
     'finding a 50$ bill on the floor',
@@ -16,8 +17,9 @@ const actions = [
     'getting asked out on a date',
     'fighthing a duel',
     'worshiping a fake god',
-    'eating spaghetti',
-    'pranking an old lady'
+    'eating mom\'s spaghetti',
+    'pranking an old lady',
+    'fighting against homer'
 ]
 const characters = [
     'SpongeBob',
@@ -36,7 +38,9 @@ const characters = [
 let mixedMessage = document.getElementById('mixedMessage');
 let button = document.getElementById('messageButton');
 
+
 function messageSelector() {
+
 var places1 = places[Math.floor(Math.random()*places.length)];
 var actions1 = actions[Math.floor(Math.random()*actions.length)];
 var characters1 = characters[Math.floor(Math.random()*characters.length)];
@@ -45,9 +49,50 @@ return "You will be in " + places1 + " " + actions1 + " with " + characters1;
 
 }
 
-function showMessage() {
-    mixedMessage.innerHTML = messageSelector();
-    button.style.cursor = 'default';
+function imageSelector(message) {
+
+
+    if (message.includes('Gotham') && message.includes('Batman')) {
+        batmanImg.style.display = 'block';
+        eminemImg.style.display = 'none';
+        peterImg.style.display = 'none';
+        return true;
+    }
+    else if (message.includes('eating mom\'s spaghetti') && message.includes('Eminem')) {
+        eminemImg.style.display = 'block';
+        peterImg.style.display = 'none';
+        batmanImg.style.display = 'none';
+        return true;
+    }
+    else if (message.includes('Springfield') && message.includes('fighting against homer') && message.includes('Peter Griffin')) {
+        peterImg.style.display = 'block';
+        eminemImg.style.display = 'none';
+        batmanImg.style.display = 'none';
+        return true;
+    }
+    else {
+        batmanImg.style.display = 'none';
+        peterImg.style.display = 'none';
+        eminemImg.style.display = 'none';
+        return false;
+    }
+
+}
+
+async function showMessage() {
+
+    var count = 0;
+    var isImage = false;
+    while (isImage === false) {
+        var message = messageSelector();
+        mixedMessage.innerHTML = message;
+        isImage = imageSelector(message);
+        batmanImg.innerHTML = imageSelector(message);
+        button.style.cursor = 'default';
+        count++;
+        console.log(count + ' ' + message);
+        await new Promise(resolve => setTimeout(resolve, 100));
+    }
 }
 
 button.addEventListener('click', showMessage);
